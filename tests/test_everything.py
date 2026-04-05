56833c0cd103bf623628c06aff05cd22c16b744c
hulc <3314986382@qq.com>
2026-04-05 13:01:43 +0800

    [TestEverything] fix import errer

--- tests/test_everything.py
+++ tests/test_everything.py
@@ -1,8 +1,6 @@
-import platform, subprocess, os
-from test_py import main as python_core_full_test
-from test_cpp import main as cpp_core_full_test
-from cpp_unit_tests import bvr_sim_unit_tests as cxx_unit_tests
-from cpp_unit_tests import c3u_unit_tests
+import platform
+import subprocess
+import os
 
 def main():
     if platform.system() == "Windows":
@@ -11,20 +9,24 @@ def main():
     else:
         # run ../bvr_sim/build_linux.sh
         subprocess.run([os.path.join(os.path.dirname(__file__), "../bvr_sim/build_linux.sh")], check=True)
-    
+
     # run pip install ..
     subprocess.run(["pip", "install", os.path.join(os.path.dirname(__file__), "../")], check=True)
 
     print("\nRunning C++ unit tests...")
+    from cpp_unit_tests import bvr_sim_unit_tests as cxx_unit_tests
     unit_test_result = cxx_unit_tests()
-    
+
     print("\nRunning C3Utils unit tests...")
+    from cpp_unit_tests import c3u_unit_tests
     c3u_test_result = c3u_unit_tests()
 
     print("\nRunning C++ core full test...")
+    from test_cpp import main as cpp_core_full_test
     cpp_result = cpp_core_full_test()
 
     print("\nRunning Python core full test...")
+    from test_py import main as python_core_full_test
     python_result = python_core_full_test()
 
     if python_result == 0 and cpp_result == 0 and unit_test_result == 0 and c3u_test_result == 0:
