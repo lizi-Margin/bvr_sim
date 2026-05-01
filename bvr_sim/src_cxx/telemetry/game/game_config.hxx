ec11fcf19999a6a3e63c2090bd480e28c11f155d
lizi-Margin <sunhaocheng@bupt.edu.cn>
2026-05-01 12:48:47 +0800

    [GameMode] 0.4.5

---
+++ bvr_sim/src_cxx/telemetry/game/game_config.hxx
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace bvr_sim {
+
+struct GameCameraConfig {
+    static constexpr float k_min_distance = 600.0f;
+    static constexpr float k_max_distance = 180000.0f;
+    static constexpr float k_follow_height_ratio = 0.6f;
+};
+
+} // namespace bvr_sim
